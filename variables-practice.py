25081984b7047995f6900143c46f556f37f5f7df
hallma886 <hallma886@northwested.org>
2024-09-06 13:31:29 -0400

    Update variables-practice.py

--- variables-practice.py
+++ variables-practice.py
@@ -1,4 +1,4 @@
-# Mike Jones
+# Matthew Hall
 # 04 SEP 2024
 # Variables Practice
 
@@ -19,9 +19,9 @@
 # Define a variable called greeting and assign it a string value you'd use to greet someone (Good morning, Good afternoon, etc.)
 # Use the print function to print the value of one of three variables you created in Part 1
 school = 'Career Tech'
-
-
-
+favorite_singer = "Michael Jackson"
+greeting = "Good Morning'
+print (greeting) function
 # Part 2 / Working with Numbers (Numeric Data)
 # Define a variable called my_age and assign it your age as an integer
 # Define a variable called conversion_factor and assign it the value of 2.54 (a float/floating-point number)
